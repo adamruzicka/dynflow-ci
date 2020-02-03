62638ce42367f4ca99fc83f2e23d7dc6a93daf71
Adam Ruzicka <aruzicka@redhat.com>
2020-02-03 15:30:23 +0100

    Drop actions

--- lib/dynflow-ci.rb
+++ lib/dynflow-ci.rb
@@ -1,7 +1,3 @@
 module DynflowCI
-  require 'dynflow-ci/base'
-  require 'dynflow-ci/stuck'
-  require 'dynflow-ci/suspend'
-  require 'dynflow-ci/polling'
-  require 'dynflow-ci/run_command'
+  require 'dynflow-ci/systemd_helper'
 end
--- lib/dynflow-ci/run_command.rb
+++
@@ -1,9 +0,0 @@
-module DynflowCI
-  class RunCommand < Base
-    def run
-      Dir.chdir(input[:path]) do
-        raise "Command failed" unless system(input[:command])
-      end
-    end
-  end
-end
