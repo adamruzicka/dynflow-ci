d7320c10e9c81602d7125327425172c2d62b6de7
Adam Ruzicka <aruzicka@redhat.com>
2020-02-03 14:10:16 +0100

    Rework systemd helper

--- lib/dynflow-ci/systemd_helper.rb
+++ lib/dynflow-ci/systemd_helper.rb
@@ -2,41 +2,37 @@ module DynflowCI
   class SystemdHelper
     class << self
       def start(cmd)
-        _, err = Open3.capture3("systemd-run --user --remain-after-exit -p WorkingDirectory=#{Dir.getwd} #{cmd}")
+        File.write('runme.sh', cmd)
+        _, err = Open3.capture3("systemd-run --user --remain-after-exit -p WorkingDirectory=#{Dir.getwd} /bin/sh #{Dir.getwd}/runme.sh")
         /(run-.*\.service)/.match(err)[1]
       end
 
-      def status(job)
-        show(job)["SubState"]
-      end
-
-      def output(job)
-        `journalctl --user-unit #{job}`
-      end
-
-      def running?(job)
-        status(job) == 'running'
-      end
-
-      def exitcode(job)
-        show(job)["ExecMainStatus"].to_i
-      end
-
       def stop(job)
+        `systemctl --user reset--failed #{job}`
         `systemctl --user stop #{job}`
       end
-
-      def log(job)
-        `journalctl --user-unit #{job}`.lines.map(&:chomp)
-      end
-
-      private
 
       def show(job)
         `systemctl show --user #{job}`.lines.reduce({}) do |acc, cur|
           key, value = cur.split('=')
           acc.merge(key => value.chomp)
         end
+      end
+
+      def log(job)
+        `journalctl --user-unit #{job}`.lines.map(&:chomp)
+      end
+
+      def status(hash)
+        hash["SubState"]
+      end
+
+      def running?(hash)
+        status(hash) == 'running'
+      end
+
+      def exitcode(hash)
+        hash["ExecMainStatus"].to_i
       end
     end
   end
